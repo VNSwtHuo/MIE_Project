cad0c72041656f91bf03abc5780ad40de3b3db1b
Vanessa Huo <vanessa.wt.huo@gmail.com>
2026-02-19 02:11:50 -0500

    Fixed bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import App from "@/components/App";
 
+// Disable static prerendering since this page requires client-side Firebase
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return <App />;
 }
